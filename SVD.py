3d8e0c061ea20d3520d13fc441f7777600d6aea5
mikexie <mxie622@gmail.com>
2019-02-12 05:48:31 +1300

    SVD Algorithm

---
+++ SVD.py
@@ -0,0 +1,24 @@
+def SVD(M):
+    import numpy as np
+    if isinstance(M, np.matrix) or isinstance(M, np.array):
+#        M = np.mat([[3, 2, 2], [2, 3, -2]])
+        U = np.linalg.eig(M * M.T) # U = M * M^T
+
+        V = np.linalg.eig(M.T * M) # Vt = M^T * M
+
+        U = U[1]
+
+        sigma = U.I * M * V[1].I
+    return U, sigma, V[1]
+
+
+import numpy as np
+# np.mat([[1, 2, 3], [4, 5, 6]])
+a = SVD(M = np.mat([[3, 2, 2], [2, 3, -2]]))
+
+print(a)
+print(a[0] * a[1] * a[2])
+
+
+
+
