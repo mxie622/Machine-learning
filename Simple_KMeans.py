11f06daffaf9e0228697e460386b009bdbc23e33
mikexie <mxie622@gmail.com>
2019-03-19 16:02:32 +1300

    Simple KMeans: Unsupervised learning

--- Simple_KMeans.py
+++ Simple_KMeans.py
@@ -37,8 +37,11 @@ def kMeans(dataSet, k, distMeas=distEclud, createCent=randCent):
                     minDist = distJI; minIndex = j
             if clusterAssment[i,0] != minIndex: clusterChanged = True
             clusterAssment[i,:] = minIndex,minDist**2
-        print centroids
+        print(centroids)
         for cent in range(k):#recalculate centroids
             ptsInClust = dataSet[nonzero(clusterAssment[:,0].A==cent)[0]]#get all the point in this cluster
             centroids[cent,:] = mean(ptsInClust, axis=0) #assign centroid to mean
     return centroids, clusterAssment
+
+
+
